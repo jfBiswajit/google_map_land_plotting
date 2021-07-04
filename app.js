dbb2500360fd3113ec811e134f0b27b4f963ea6e
Biswajit Biswas <jfbiswajit@gmail.com>
2021-07-04 14:01:17 +0600

    Add: works as expected

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=drawing">
 function initMap() {
   const map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 24.886, lng: -70.268 },
-    zoom: 5,
+    center: { lat: 23.93567, lng: 89.08797 },
+    zoom: 20,
   });
 
   const drawingManager = new google.maps.drawing.DrawingManager({
@@ -48,24 +48,19 @@ function initMap() {
       
       console.log(JSON.stringify(landShape));
       
-      map.setCenter({
-        lat: 23.8103,
-        lng: 90.4125,
-      });
+      // map.setCenter({
+      //   lat: 23.8103,
+      //   lng: 90.4125,
+      // });
     }
   );
 
   // Define the LatLng coordinates for the polygon's path.
   const triangleCoords = [
-    { lat: 31.82115566688796, lng: -75.277765625 },
-    { lat: 27.46886055111971, lng: -74.5306953125 },
-    { lat: 28.82500382433339, lng: -80.99065625 },
-    { lat: 30.996033438254603, lng: -77.9584296875 },
-    { lat: 31.97039574462498, lng: -83.1439765625 },
-    { lat: 33.37601053783669, lng: -80.4193671875 },
-    { lat: 32.30529932423105, lng: -65.7416328125 },
-    { lat: 31.03369604870428, lng: -71.937921875 },
-    { lat: 31.97039574462498, lng: -74.48675 },
+    { lat: 23.935707144748452, lng: 89.08784488577032 },
+    { lat: 23.935721854002622, lng: 89.08810371894026 },
+    { lat: 23.935631146908552, lng: 89.08810774225378 },
+    { lat: 23.93561643764405, lng: 89.08785293239737 },
   ];
 
   // Construct the polygon.
