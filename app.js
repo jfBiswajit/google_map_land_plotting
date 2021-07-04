10aa273d6a57d8dd7f2b34aabe93a35dae5cea82
Biswajit Biswas <jfbiswajit@gmail.com>
2021-07-04 11:44:19 +0600

    Add: works as expected

--- app.js
+++ app.js
@@ -2,29 +2,25 @@
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=drawing">
 function initMap() {
-  
   const map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
-  
+
   const drawingManager = new google.maps.drawing.DrawingManager({
     drawingMode: google.maps.drawing.OverlayType.MARKER,
     drawingControl: true,
     drawingControlOptions: {
       position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: [
-        google.maps.drawing.OverlayType.POLYGON,
-        google.maps.drawing.OverlayType.RECTANGLE,
-      ],
+      drawingModes: [google.maps.drawing.OverlayType.POLYGON],
     },
   });
-  
+
   drawingManager.setMap(map);
-  
+
   // Testing
   var polylines = [];
-  
+
   google.maps.event.addListener(
     drawingManager,
     'polygoncomplete',
@@ -34,14 +30,6 @@ function initMap() {
       var polylinePath = polygon.getPath();
       var testPath = polylinePath.getArray();
       console.log(testPath);
-    }
-  );
-  
-  google.maps.event.addListener(
-    drawingManager,
-    'rectanglecomplete',
-    function (rectangle) {
-      console.log('rectangle complete');
     }
   );
 }
